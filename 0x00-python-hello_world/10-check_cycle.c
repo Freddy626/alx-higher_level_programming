c81884b2f4c546c30265f4c21f3c8a74417060e1
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-11-28 14:43:44 -0800

    ff

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-* _realloc - Reallocates a memory block
-* @ptr: The pointer to the previous memory block
-* @old_size: The size of the old memory block
-* @new_size: The size of the new memory block
-*
-* Return: The pointer to the new memory block otherwise NULL
-*/
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *new_ptr;
@@ -40,10 +40,10 @@ return (new_ptr);
 }
 }
 /**
-* check_cycle - checks if a singly linked list has a cycle in it
-* @list: list head
-* Return: 0 if no, 1 if yes
-*/
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list head
+ * Return: 0 if no, 1 if yes
+ */
 int check_cycle(listint_t *list)
 {
 listint_t *fast, *slow = list;
