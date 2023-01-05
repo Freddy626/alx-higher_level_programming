8420619881c9a53d37a7fa986f56a44ae851d79f
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2023-01-04 19:15:25 -0800

    ff

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,6 @@
 """Defines a Rectangle class."""
 class Rectangle:
     """Represent a rectangle."""
-
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
